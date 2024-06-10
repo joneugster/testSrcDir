5c922f17a89baa8bf6b6adf1413b49a1a1646a0c
Jon Eugster <eugster.jon@gmail.com>
2024-06-10 16:48:38 +0200

    .

--- TestSrcDir/Main.lean
+++ TestSrcDir/Main.lean
@@ -11,7 +11,7 @@ def main : IO Unit := do
   let sp ← searchPathRef.get
   -- IO.println s!"Search path: {sp}"
 
-  let module := `UpdateDeprecations.Main
+  let module := `TestSrcDir.Main
   let mFile ← findOLean module
 
   IO.println s!"Hello! {hello}"
