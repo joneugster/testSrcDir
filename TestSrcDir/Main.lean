396c471b26703ed0b0c6505bd5c089ca8beda7f8
Jon Eugster <eugster.jon@gmail.com>
2024-06-10 16:47:30 +0200

    .

--- TestSrcDir/Main.lean
+++ TestSrcDir/Main.lean
@@ -1,4 +1,17 @@
 import TestSrcDir.Basic
+import Lean
 
-def main : IO Unit :=
+open Lean
+
+def main : IO Unit := do
+  Lean.initSearchPath (← Lean.findSysroot)
+
+  IO.println s!"current dir: {← IO.currentDir}"
+
+  let sp ← searchPathRef.get
+  -- IO.println s!"Search path: {sp}"
+
+  let module := `UpdateDeprecations.Main
+  let mFile ← findOLean module
+
   IO.println s!"Hello! {hello}"
